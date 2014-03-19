41d1caa2d8b403ec0500e9502aeed85f201c4a06
firstrow <firstrow@gmail.com>
2014-03-19 20:22:28 +0200

    Added simple docblock

--- src/FeatureScience/Test.php
+++ src/FeatureScience/Test.php
@@ -2,6 +2,31 @@
 
 namespace FeatureScience;
 
+/**
+ * Main class to run tests.
+ *
+ * Configuring.
+ * <pre>
+ * 		\FeatureScience\DI::set([
+ *			'storage'=> new \FeatureScience\FileStorage('/tmp');
+ * 		]);
+ * </pre>
+ * NOTE: By default FeatureScience uses `FileStorage` class.
+ *
+ * Support config options:
+ *  - storage: Class to temporary store expetiment results.
+ *
+ * Example usage:
+ * <pre>
+ *		$experiment = new \FeatureScience\Test('file.save', [
+ *			'control'   => function() { return $this->oldMethod(); },
+ *			'candidate' => function() { return $this->newMethod(); }
+ *		]);
+ *
+ * 		// Will return randomly selected lambda and save results to storage.
+ *		$experiment->run();
+ * </pre>
+ */
 class Test
 {
 
